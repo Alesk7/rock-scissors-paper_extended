aa7f8a50922ae1345777cc5772c64f3cb90be5ad
Alesk1 <zxcv9167@gmail.com>
2018-01-14 23:12:54 +0300

    Добавьте файлы проекта.

---
+++ rock-scissors-paper_extended/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rock_scissors_paper_extended
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
